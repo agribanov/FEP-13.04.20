8ead333e509adde6198d9420af15af3933b67e07
Aleksandr Gribanov <aleksandr.gribanov@gmail.com>
2020-05-25 19:16:21 +0300

    Lection

--- script.js
+++ script.js
@@ -16,3 +16,12 @@ console.log(counter10.count());
 console.log(counter10.count());
 
 counter10 = null;
+
+// ДЗ
+const calc = createCalculator(34);
+
+calc.sum(2345); /// 15
+calc.mult(10); // 150
+calc.sub(40); // 110
+calc.div(10); // 11
+calc.set(100); //
