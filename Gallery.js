1b624e3adc84d1b39072b66aa6b7889cc1d4dd31
Aleksandr Gribanov <aleksandr.gribanov@gmail.com>
2020-06-05 19:38:57 +0300

    Lection

---
+++ Gallery.js
@@ -0,0 +1,52 @@
+function Gallery(containerEl, photos) {
+    this.containerEl = containerEl;
+
+    this.currentPhoto = -1;
+
+    this.generateHtml(photos);
+    this.showNextPhoto();
+    setInterval(this.showNextPhoto.bind(this), 2000);
+}
+
+Gallery.VISIBLE_CLASS = 'visible';
+
+Gallery.prototype.generateHtml = function (photos) {
+    const lis = photos.map(
+        (photo, index) =>
+            `<li class="gallery-photo"><img src="${photo}?${index}" /></li>`
+    );
+
+    this.rootElement = document.createElement('ul');
+    this.rootElement.insertAdjacentHTML('beforeEnd', `${lis.join('')}`);
+
+    this.containerEl.append(this.rootElement);
+
+    this.rootElement.addEventListener(
+        'click',
+        this.onRootElementClick.bind(this)
+    );
+};
+
+Gallery.prototype.onRootElementClick = function (e) {
+    console.log('clicked on ul');
+
+    this.showNextPhoto();
+};
+
+Gallery.prototype.showNextPhoto = function () {
+    if (this.rootElement.children[this.currentPhoto]) {
+        this.rootElement.children[this.currentPhoto].classList.remove(
+            Gallery.VISIBLE_CLASS
+        );
+    }
+
+    this.currentPhoto++;
+
+    if (this.currentPhoto === this.rootElement.children.length) {
+        this.currentPhoto = 0;
+    }
+
+    this.rootElement.children[this.currentPhoto].classList.add(
+        Gallery.VISIBLE_CLASS
+    );
+};
--- script.js
+++ script.js
@@ -1,27 +1,14 @@
-function createrCounter(counts) {
-    return {
-        count: function () {
-            return ++counts;
-        },
-    };
-}
+const photos = [
+    'https://picsum.photos/200/300',
+    'https://picsum.photos/200/300',
+    'https://picsum.photos/200/300',
+    'https://picsum.photos/200/300',
+    'https://picsum.photos/200/300',
+    'https://picsum.photos/200/300',
+    'https://picsum.photos/200/300',
+    'https://picsum.photos/200/300',
+    'https://picsum.photos/200/300',
+    'https://picsum.photos/200/300',
+];
 
-const counter = createrCounter(0);
-const counter10 = createrCounter(10);
-
-console.log(counter.count());
-console.log(counter.count());
-
-console.log(counter10.count());
-console.log(counter10.count());
-
-counter10 = null;
-
-// ДЗ
-const calc = createCalculator(34);
-
-calc.sum(2345); /// 15
-calc.mult(10); // 150
-calc.sub(40); // 110
-calc.div(10); // 11
-calc.set(100); //
+new Gallery(document.getElementById('container'), photos);
