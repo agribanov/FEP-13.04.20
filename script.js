a621afe2c6fe7777d7ab6db101c11b7c011695b5
Aleksandr Gribanov <aleksandr.gribanov@gmail.com>
2020-06-23 09:08:37 +0300

    Lection

--- script.js
+++ script.js
@@ -1,41 +1,44 @@
-// HTTP
-// HTTPS
-// FTP
-// SFTP
+USER_URL = 'https://jsonplaceholder.typicode.com/users';
 
-// GET
+fetch(USER_URL)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 
-// POST
-// PUT
+let thirdUser;
+fetch(USER_URL + '/' + 3)
+    .then((res) => res.json())
+    .then((data) => (thirdUser = data));
+// .then(updateUser);
 
-// DELETE
+const user = {
+    name: 'Alex',
+    surname: 'Smith',
+};
 
-// HTTP Request
-//     URL
-//         protocol
-//         domain (host, hostName)
-//         path
-//         queryParams
-//     METHOD
-//         GET, POST, PUT, DELETE
-//     BODY
-//         Data
-//     HEADERS
+fetch(USER_URL + '/' + 3, {
+    method: 'DELETE',
+});
 
-// HTTP Response
-//     Data
-//     STATUS
-//     Response
+// fetch(USER_URL, {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(user),
+// })
+//     .then((res) => res.json())
+//     .then((data) => console.log(data));
 
-// DNS
+// function updateUser() {
+//     thirdUser.name = 'Alex';
 
-// AJAX
-// JSON
-
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then((res) => {
-        return res.json();
-    })
-    .then((data) => {
-        console.log(data);
-    });
+//     fetch(USER_URL + '/' + 3, {
+//         method: 'PUT',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(thirdUser),
+//     })
+//         .then((res) => res.json())
+//         .then((data) => console.log(data));
+// }
